d9e388fee43864a2cf6174c1428a396556644a70
NimishaMN <nimisha.mn@gmail.com>
2018-11-26 11:14:52 +0530

    ignore

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :publishers
   resources :authors
   resources :books
+  root to: 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
